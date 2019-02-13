aee501ae2f19d775ad430132adbc9627d6f4498d
Matt <Mtynan4@gmail.com>
2019-02-13 21:59:21 +0000

    pre save hook to hash the password

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 
 const userSchema = new mongoose.Schema({
@@ -12,6 +13,21 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+//hooks before the save, before we save this we run this async function
+userSchema.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) { //if this user has not modified the pw then move on using next()
+            return next();
+        }
+        let hashedPassword = await bcrypt.hash(this.password, 10); //hashes the pw
+        this.password = hashedPassword; //sets the pw to the hashed pw before we save in db
+        return next(); //used to move on
+    } catch (err) {
+        return next(err); //catches error for error handler
+    }
+});
+
 
 const User = mongoose.model("User", userSchema);
 
