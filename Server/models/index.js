cc6df6e4db525bae424d6a7f4185ef64ec8b760f
Matt <Mtynan4@gmail.com>
2019-02-13 21:10:56 +0000

    connecting to mongodb

---
+++ Server/models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/test7", {
+    keepAlive: true,
+    useNewUrlParser: true
+});
+
+
+
+//connects to db and sets up mongo
+//Promise used so we don't use callback pattern, async instead
